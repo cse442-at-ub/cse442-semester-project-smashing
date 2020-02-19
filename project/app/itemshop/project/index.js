07559a75ebda944f0c319c190c675680b2a466e0
Justin Whitehead <jrwhiteh@buffalo.edu>
2020-02-19 14:26:35 -0500

    implemented item shop button #50

---
+++ project/app/itemshop/project/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+
+export default class ItemShop extends Component {
+  render() {
+    return (
+      <View>
+        {/* <Text>Item Shop!</Text> */}
+      </View>
+    );
+  }
+}
