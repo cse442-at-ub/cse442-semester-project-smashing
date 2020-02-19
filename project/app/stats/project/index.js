53710fa1b7e128387d07df0b608216a059ce9c59
Justin Whitehead <jrwhiteh@buffalo.edu>
2020-02-19 14:53:15 -0500

    implemented stats screenbutton #50

---
+++ project/app/stats/project/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+
+export default class Stats extends Component {
+  render() {
+    return (
+      <View>
+        {/* <Text>Stats Go Here!</Text> */}
+      </View>
+    );
+  }
+}
