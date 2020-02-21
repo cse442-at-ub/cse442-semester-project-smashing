c256012e69b8f2152779fbb498015ac4bd898233
Vikram Singh <vikzilla18@gmail.com>
2020-02-20 22:05:07 -0500

    Rendered mock data into list #23

--- project/app/itemshop/project/index.js
+++ project/app/itemshop/project/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 
 // Styles corresponding to the different views on the page
 const viewStyles = StyleSheet.create({
@@ -7,8 +7,7 @@ const viewStyles = StyleSheet.create({
     flex: 1
   },
   Body: {
-    flex: 8,
-    backgroundColor: "blue"
+    flex: 8
   },
   Footer: {
     flex: 1,
@@ -17,48 +16,65 @@ const viewStyles = StyleSheet.create({
 });
 
 const headerStyles = StyleSheet.create({
-  Title:{
+  Title: {
     textAlign: "center",
     justifyContent: "center",
     fontSize: 30,
     fontWeight: "bold"
   }
-})
+});
 
 // Store data
 const mockItemStoreData = [
   {
-    id: 1,
+    id: "1",
     name: "Large Couch"
   },
   {
-    id: 2,
+    id: "2",
     name: "Blue Shirt"
   },
   {
-    id: 3,
+    id: "3",
     name: "Brick Wallpaper"
   },
   {
-    id: 4,
+    id: "4",
     name: "Top Hat"
-  },
+  }
 ];
 
-export default class ItemShop extends Component {
+// Creates a list item from a set of props
+function ListItem({name}) {
+  return (
+    <View>
+      <Text>{name}</Text>
+    </View>
+  );
+}
 
+export default class ItemShop extends Component {
   render() {
     return (
       <View style={{ flex: 1 }}>
-        
         <View style={viewStyles.Header}>
           <Text style={headerStyles.Title}>Item Shop!</Text>
         </View>
 
-        <View style={viewStyles.Body} />
+        <View style={viewStyles.Body}>
+          <FlatList
+            data={mockItemStoreData}
+            renderItem={({item}) => 
+              <ListItem name={item.name} />
+            }
+            keyExtractor={item =>
+              item.id
+            }
+          />
+        </View>
+
         <View style={viewStyles.Footer} />
       </View>
     );
   }
-
 }
