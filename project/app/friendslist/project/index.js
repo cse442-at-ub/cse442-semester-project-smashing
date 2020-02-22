bf268d7809b700c2c96c9abf888931c323497cf3
Matthew Charland <ny.matt.charland3@gmail.com>
2020-02-22 14:31:21 -0500

    a title that says user name #56

--- project/app/friendslist/project/index.js
+++ project/app/friendslist/project/index.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 
 export default class FriendsList extends Component {
   render() {
     return (
-      <View>
-        {/* <Text>Friends List Here </Text> */}
+      <View style={styles.container}>
+          <View style={styles.tabView}>
+          <Text style={styles.userName}>User Name</Text>
+          </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  tabView: {
+    color: 'black',
+    backgroundColor: 'white',
+    height: '8%',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+  userName: {
+    color: 'black',
+    flex: 2,
+    paddingLeft: 50,
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+});
