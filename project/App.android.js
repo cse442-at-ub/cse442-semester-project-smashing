01e5b4a188632de45b312d4b94fc11335bed791f
TravisBute <trb0814@gmail.com>
2020-03-19 12:16:19 -0400

    fix for android

--- project/App.android.js
+++ project/App.android.js
@@ -9,26 +9,28 @@ import FriendsList from "./app/friendslist";
 import ItemShop from "./app/itemshop";
 import Stats from "./app/stats";
 import CalorieCounter from "./app/caloriecounter"
-
-
-
+import Login from "./app/loginpage"
+import Register from "./app/register"
 
 EStyleSheet.build();
+var validLogin = false
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       sceneVisible: false,
-      scene: null
+      scene: null,
+      loggedin: false,
     };
+    this.callbackFunction = this.callbackFunction.bind(this);
   }
 
   mountScene = scene => {
     this.setState({
       sceneVisible: true,
       scene: scene
-      
+
     });
   };
 
@@ -39,36 +41,99 @@ export default class App extends Component {
     });
   };
 
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <TableOfContents
-          sceneVisible={this.state.sceneVisible}
-          contents={{
-            heading: "Smashing",
-            items: [
-              Start(this.mountScene),
-              LeaderBoards(this.mountScene),
-	            FriendsList(this.mountScene),
-              ItemShop(this.mountScene),
-              Stats(this.mountScene),
-              CalorieCounter(this.mountScene),
-            ]
-          }}
-        />
-        <Modal
-        
-          animationType={"slide"}
-          transparent={false}
-          visible={this.state.sceneVisible}
-          onRequestClose={_ => {}}
-        >
-          {this.state.scene}
+  callbackFunction = (loginData) => {
+    console.log(loginData);
+    console.log("calling callback function");      
+    this.setState({loggedin: loginData})
+    
+    this.unMountScene();
+    this.mountScene(<TableOfContents
+      sceneVisible={this.state.sceneVisible}
+      contents={{
+        heading: "Smashing",
+        items: [
+          Start(this.mountScene),
+          LeaderBoards(this.mountScene),
+          FriendsList(this.mountScene),
+          ItemShop(this.mountScene),
+          Stats(this.mountScene),
+          CalorieCounter(this.mountScene),
+        ]
+      }}
+    />);
+  };
 
-          <BackButton onPress={this.unMountScene} />
-        </Modal>
-        
-      </View>
-    );
+  render() {
+    console.log(this.state.loggedin);
+    if (this.state.loggedin === true){
+      validLogin = true;
+      console.log("CHANGING")
+    } else {
+      validlogin = false;
+    }
+
+    if (validLogin === true) {
+      console.log("Valid Login");
+      return (
+        <View style={{ flex: 1 }}>
+          <TableOfContents
+            sceneVisible={this.state.sceneVisible}
+            contents={{
+              heading: "Smashing",
+              items: [
+                Start(this.mountScene),
+                LeaderBoards(this.mountScene),
+                FriendsList(this.mountScene),
+                ItemShop(this.mountScene),
+                Stats(this.mountScene),
+                CalorieCounter(this.mountScene),
+              ]
+            }}
+          />
+          <Modal
+
+            animationType={"slide"}
+            transparent={false}
+            visible={this.state.sceneVisible}
+            onRequestClose={_ => { }}
+          >
+            {this.state.scene}
+
+            <BackButton onPress={this.unMountScene} />
+          </Modal>
+
+        </View>
+      );
+    }
+    else{
+      console.log("Not Valid Login");
+      return (
+        <View style={{ flex: 1 }}>
+          <TableOfContents
+            sceneVisible={this.state.sceneVisible}
+            contents={{
+              heading: "Smashing",
+              items: [
+                Login(this.mountScene, this.callbackFunction),
+                Register(this.mountScene),
+              ]
+            }}
+          />
+          <Modal
+
+            animationType={"slide"}
+            transparent={false}
+            visible={this.state.sceneVisible}
+            onRequestClose={_ => { }}
+          >
+            {this.state.scene}
+
+            <BackButton onPress={this.unMountScene} />
+          </Modal>
+
+        </View>
+      );
+    }
+    
   }
 }
