e969a82e972b0d525cbe72b4b47caef98ca7087c
Vikram Singh <vikzilla18@gmail.com>
2020-03-19 14:38:38 -0400

    Updated the back button to return to the correct page. #77

--- project/App.android.js
+++ project/App.android.js
@@ -1,139 +1,103 @@
 import React, { Component } from "react";
-import { View, Modal } from "react-native";
+
+import { View, Text, Modal } from "react-native";
+
 import BackButton from "./app/table-of-contents/backbutton";
 import EStyleSheet from "react-native-extended-stylesheet";
 import TableOfContents from "./app/table-of-contents";
-import Start from "./app/start";
-import LeaderBoards from "./app/leaderboards";
+
+import CalorieCounter from "./app/caloriecounter"
 import FriendsList from "./app/friendslist";
 import ItemShop from "./app/itemshop";
-import Stats from "./app/stats";
-import CalorieCounter from "./app/caloriecounter"
+import LeaderBoards from "./app/leaderboards";
 import Login from "./app/loginpage"
 import Register from "./app/register"
+import Start from "./app/start";
+import Stats from "./app/stats";
 
 EStyleSheet.build();
-var validLogin = false
 
 export default class App extends Component {
+
   constructor(props) {
     super(props);
+   
     this.state = {
-      sceneVisible: false,
-      scene: null,
-      loggedin: false,
+      loggedIn: false,
+      scene: this.loggedOutView(),
     };
+
     this.callbackFunction = this.callbackFunction.bind(this);
+    this.mountScene = this.mountScene.bind(this);
+    this.unMountScene = this.unMountScene.bind(this);
   }
 
   mountScene = scene => {
-    this.setState({
-      sceneVisible: true,
-      scene: scene
+    console.log("mounting scene");
+    console.log(scene);
 
+    this.setState({
+      scene: scene
     });
   };
 
   unMountScene = () => {
-    this.setState({
-      sceneVisible: false,
-      scene: null
-    });
+    const {loggedIn} = this.state;
+
+    const nextScene = loggedIn? this.loggedInView(): this.loggedOutView();
+    this.mountScene(nextScene)
   };
 
   callbackFunction = (loginData) => {
-    console.log(loginData);
-    console.log("calling callback function");      
-    this.setState({loggedin: loginData})
-    
+    this.setState({loggedIn: loginData});
+
+    // Unmount the scene to return to the table of contents
     this.unMountScene();
-    this.mountScene(<TableOfContents
-      sceneVisible={this.state.sceneVisible}
-      contents={{
-        heading: "Smashing",
-        items: [
-          Start(this.mountScene),
-          LeaderBoards(this.mountScene),
-          FriendsList(this.mountScene),
-          ItemShop(this.mountScene),
-          Stats(this.mountScene),
-          CalorieCounter(this.mountScene),
-        ]
-      }}
-    />);
   };
 
   render() {
-    console.log(this.state.loggedin);
-    if (this.state.loggedin === true){
-      validLogin = true;
-      console.log("CHANGING")
-    } else {
-      validlogin = false;
-    }
+    const {scene} = this.state;
+    return (
+        <>
+          {scene}
+          <BackButton onPress={()=>{console.log("brain");this.unMountScene()}} />
+        </>
+    );
+  }
 
-    if (validLogin === true) {
-      console.log("Valid Login");
-      return (
-        <View style={{ flex: 1 }}>
-          <TableOfContents
-            sceneVisible={this.state.sceneVisible}
-            contents={{
-              heading: "Smashing",
-              items: [
-                Start(this.mountScene),
-                LeaderBoards(this.mountScene),
-                FriendsList(this.mountScene),
-                ItemShop(this.mountScene),
-                Stats(this.mountScene),
-                CalorieCounter(this.mountScene),
-              ]
-            }}
-          />
-          <Modal
+  loggedInView = ()=>{
+    return (
+      <View style={{ flex: 1 }}>
+        <TableOfContents
+          contents={{
+            heading: "Smashing",
+            items: [
+              Start(this.mountScene),
+              LeaderBoards(this.mountScene),
+              FriendsList(this.mountScene),
+              ItemShop(this.mountScene),
+              Stats(this.mountScene),
+              CalorieCounter(this.mountScene),
+            ]
+          }}
+        />
+      </View>
+    );
+  }
 
-            animationType={"slide"}
-            transparent={false}
-            visible={this.state.sceneVisible}
-            onRequestClose={_ => { }}
-          >
-            {this.state.scene}
-
-            <BackButton onPress={this.unMountScene} />
-          </Modal>
-
-        </View>
-      );
-    }
-    else{
-      console.log("Not Valid Login");
-      return (
-        <View style={{ flex: 1 }}>
-          <TableOfContents
-            sceneVisible={this.state.sceneVisible}
-            contents={{
-              heading: "Smashing",
-              items: [
-                Login(this.mountScene, this.callbackFunction),
-                Register(this.mountScene),
-              ]
-            }}
-          />
-          <Modal
-
-            animationType={"slide"}
-            transparent={false}
-            visible={this.state.sceneVisible}
-            onRequestClose={_ => { }}
-          >
-            {this.state.scene}
-
-            <BackButton onPress={this.unMountScene} />
-          </Modal>
-
-        </View>
-      );
-    }
-    
+  loggedOutView = ()=>{
+    return (
+      <View style={{ flex: 1 }}>
+        <TableOfContents
+          contents={{
+            heading: "Smashing",
+            items: [
+              Login(this.mountScene, this.callbackFunction),
+              Register(this.mountScene),
+            ]
+          }}
+        />
+      </View>
+    );
   }
 }
