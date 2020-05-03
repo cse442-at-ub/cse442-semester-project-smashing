a60e96e73f7ccdfb1e1097d3565f4d0ff3347902
Justin Whitehead <jrwhiteh@buffalo.edu>
2020-05-03 14:39:17 -0400

    #117 saved todays to redux store when logging in, grabbed from redux store and placed on calorie counter page

--- project/app/redux/reducers/user.js
+++ project/app/redux/reducers/user.js
@@ -3,6 +3,7 @@ import * as Actions from "../actionTypes";
 const initialState = {
     username: null,
     money: null,
+    todays: null,
 }
 
 export default function(state=initialState, action){
@@ -12,6 +13,8 @@ export default function(state=initialState, action){
         case Actions.USER_MONEY:
             return Object.assign({}, state, {...action.payload});
         case Actions.USER_STATS_REFRESH:
+            return Object.assign({}, state, {...action.payload});
+        case Actions.USER_TODAYS:
             return Object.assign({}, state, {...action.payload});
         default:
             return state;
